71976172f99b02f8dcd1e9bbe07ef62c2016f20c
mayank1029 <contact2mayankgupta@gmail.com>
2017-01-24 11:04:40 +0530

    Correcting branch 2

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = 8080;
 const app = express();
 app.get('/', function (req, res) {
   res.send('Branch-2');
-});
+}); 
 
 app.listen(PORT);
 console.log('Running on http://localhost:' + PORT);
