b77a2dac66a395e5f4ba434c48e62d18e5a227ad
mayank1029 <contact2mayankgupta@gmail.com>
2017-01-24 10:59:31 +0530

    Releasing 7.0

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = 8080;
 const app = express();
 app.get('/', function (req, res) {
   res.send('Ready for fourth release\n');
-});
+}); 
 
 app.listen(PORT);
 console.log('Running on http://localhost:' + PORT);
